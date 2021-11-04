69d4c292a677dead981d4d2eed8fe643eb76116a
AldrichHer <aldrich.upiicsa@gmail.com>
2021-11-04 13:57:37 -0600

    adicionando env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 8080;
+require('dotenv').config();
+const port = process.env.PORT;
 
 //servir contenido estatico 
 //sirve por defecto en "/"
